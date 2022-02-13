17935e2f4353956b686e09f39bbfd613a01a14f5
Rod Fitzgerald <RoderickAus@gmail.com>
2022-02-14 00:24:41 +1100

    Updated the scripts to take arguments. :D

--- InvestmentTargetCalc.py
+++ InvestmentTargetCalc.py
@@ -1,34 +1,39 @@
 #This is a script which you put in the amount of money that you want to earn in the future for passive income.
-#Apparently argv0 on Windows is the script itself, neato.
+#Apparently argv0 on Windows is the script itself, neato. 
+#I am debating making the same calculator but with an effective monetary value.
+#This is like when you look at how much $200 was in 1960 and you're like wow that really was a lot in todays money.
+#Similar to that as we go further in the future the more money we have the less effective it actually is.
+#Still having millions in the future is a lot of money though.
 
-import sys
+from sys import argv
 
-PayoutTarget = 400000
-InvestmentTarget = 2000000
-InvestmentInput = 2100 #Monthly
+PayoutTarget = 400000 #My target of $400,000 
+InvestmentTarget = 2000000 #Unused but I might use later.
+InvestmentInput = 2100 #Monthly, more aggressive than I think I can do.
 TotalInvested = 0
-GrowthRate = 0.09
+GrowthRate = 0.09 #This is on company bonds, some places have reporting around 9% rate which is massive.
 Payout = 0
 Years = 0
 Months = 0
 
-print(sys.argv[0])
-
-if sys.argv[1].isdigit():
-    PayoutTarget = int(sys.argv[1])
-if sys.argv[2].isdigit():
-    InvestmentInput = int(sys.argv[2])
+print(argv[0])
+if len(argv) > 1:
+    if argv[1].isdigit():
+        PayoutTarget = int(argv[1])
+if len(argv) > 2:
+    if argv[2].isdigit():
+        InvestmentInput = int(argv[2])
 
 
 while Payout < PayoutTarget :
     while Months < 12 : 
         TotalInvested = TotalInvested + InvestmentInput
         Months = Months + 1
-        #print(Months, TotalInvested)
     Months = 0
     TotalInvested = TotalInvested * (1 + GrowthRate)
     Payout = TotalInvested * (GrowthRate / 2)
     Years = Years + 1
-    #print(str(Payout) + " " + str(Years))
 
-print(str(Payout) + " " + str(Years))
+print()
+print("You have $" + '{:,}'.format(round(TotalInvested, 2)) + " invested which generates you passively, $" + '{:,}'.format(round(Payout, 2)) + " each year.")
+print("It took you a total of: " + str(Years) + " years to achieve.")
--- SDRC.py
+++ SDRC.py
@@ -1,20 +1,43 @@
 #Basic script to see how long it'll take for you to pay off your student debt if you never got a pay increase / decrease.
+from sys import argv
 
-repaymentAmount = 2275  #Change this to your yearly repayments
-totalloanAmount = 59000 #Change this to your borrowed amount.
+totalloanAmount = 60000 #Change this to your borrowed amount.
+repaymentAmount = 5000  #Change this to your yearly repayments
 indexAmount = 0.029
 daysinyear = 365
 years = 1
+freedomIsReal = True
+LongOutput = False
+
+print(argv[0])
+if len(argv) > 1:
+    if argv[1].isdigit():
+        totalloanAmount = int(argv[1])
+if len(argv) > 2:
+    if argv[2].isdigit():
+        repaymentAmount = int(argv[2])
+if len(argv) > 3:
+    LongOutput = True
+
 
 while totalloanAmount > 0 :
     i = 0
+    origianlAmount = totalloanAmount
     while i != daysinyear :
         totalloanAmount = (totalloanAmount * (1 + indexAmount / daysinyear))
         i = i + 1
-    print("Loan remaining: " + str(totalloanAmount))
-    print("Years passed: " + str(years))
+    if LongOutput : 
+        print("Loan remaining: " + str(totalloanAmount))
+        print("Years passed: " + str(years))
+    if (origianlAmount < (totalloanAmount - repaymentAmount)):
+        freedomIsReal = False
+        break
     totalloanAmount = (totalloanAmount - repaymentAmount)
-    print("Loan after payment: " + str(totalloanAmount))
+    if LongOutput: 
+        print("Loan after payment: " + str(totalloanAmount))
     years = years + 1
 
-print("It will take you " + str(years) + " at this rate to pay off your student loan.")
+if freedomIsReal == False :
+    print("\n\nThe loan will never be paid off with current repayment.")
+else:
+    print("\n\nIt will take you " + str(years) + " at this rate to pay off your student loan.")
